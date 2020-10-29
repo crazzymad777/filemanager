cf32c9fe9b21f76b2bb7858e20ad9ee57ff9c759
Юрий Москов <aerobus.778@gmail.com>
2020-10-29 15:57:42 +0300

    fix path separator

--- src/TimerTask.java
+++ src/TimerTask.java
@@ -28,7 +28,7 @@ public class TimerTask extends java.util.TimerTask {
 				for (String x : directories) {
 					File file = new File(data+x);
 					if(!file.isFile()){
-						filesTree.addObject(node,x+"\\",true,true);
+						filesTree.addObject(node,x+File.pathSeparator,true,true);
 					}else{
 						filesTree.addObject(node,x,true,false);
 					}
